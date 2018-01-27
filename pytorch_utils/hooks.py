3696fec89b368928d4469ea3526d13a224b51b77
Chris Cotter <cotter@sciencesundries.com>
2018-01-27 21:33:33 +0000

    Added hooks

---
+++ pytorch_utils/hooks.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def grad_norm(self, grad_input, grad_output):
+    '''
+        Hook for traking norm of module gradient
+
+        Example:
+
+        D = torch.Module()
+        D.register_backward_hook(hook_grad)
+
+        ## Train and call Variable().backward()
+        print(D.__grad_norm__)
+    '''
+
+    self.__grad_norm__ = np.mean([p.data.norm() for p in grad_input])
